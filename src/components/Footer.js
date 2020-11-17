c968911356fd75c7c83e96859de27b12497756e9
[Vicente G. Reyes] <[vicentereyes.freelance@gmail.com]>
2020-11-18 03:29:33 +0800

    changed copyright to studio.icvn.tech

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -202,7 +202,7 @@ export default function Footer() {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
               <div className="text-sm text-gray-600 font-semibold py-1">
-                Copyright © {new Date().getFullYear()} icvn.tech by{" "}
+                Copyright © {new Date().getFullYear()} studio.icvn.tech by{" "}
                 <a
                   href="https://www.studio.icvn.tech"
                   className="text-gray-600 hover:text-gray-900"

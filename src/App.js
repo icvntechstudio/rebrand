5372f12eaf6d8bbe6fce0411187faf84d0223467
[Vicente G. Reyes] <[vicentereyes.freelance@gmail.com]>
2020-11-18 06:17:57 +0800

    added testi comp and post.css.config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/styles/tailwind.css";
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Feature from './components/Features'
 import Clients from './components/Clients'
 import Portfolio from './components/Portfolio'
+import Testimonial from './components/Testimonial'
 
 class App extends Component{
   render() {
@@ -15,6 +18,7 @@ class App extends Component{
         <Feature />
         <Clients />
         <Portfolio />
+        <Testimonial />
         <Footer />
       </div>
       )
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,7 +6,7 @@ class Portfolio extends Component {
 	render() {
 		return (
 			<>
-	            <div className="justify-center text-center flex flex-wrap mt-24">
+	            <div className="justify-center text-center flex flex-wrap mt-12">
 		            <div className="w-full md:w-6/12 px-12 md:px-4">
 		              <h2 className="font-semibold text-4xl">Beautiful Example Pages</h2>
 		              <p className="text-lg leading-relaxed mt-4 mb-20 text-gray-600">
@@ -30,7 +30,7 @@ class Portfolio extends Component {
 				                        <img
 				                          alt="..."
 				                          className="align-middle border-none max-w-full h-auto rounded-lg"
-				                          src={require("assets/img/login.jpg")}
+				                          src="https://demos.creative-tim.com/notus-react/static/media/login.a6f0905b.jpg"
 				                        />
 				                      </div>
 				                    </Link>
@@ -45,13 +45,13 @@ class Portfolio extends Component {
 				                        <img
 				                          alt="..."
 				                          className="align-middle border-none max-w-full h-auto rounded-lg"
-				                          src={require("assets/img/profile.jpg")}
+				                          src="https://demos.creative-tim.com/notus-react/static/media/profile.056db542.jpg"
 				                        />
 				                        </div>
 				                    </Link>
 			                    </div>
 
-			                    <div className="w-full lg:w-4/12 px-4 ">
+			                    <div className="w-full lg:w-4/12 px-4">
 				                    <h5 className="text-xl font-semibold pb-4 text-center">
 				                      Landing Page
 				                    </h5>
@@ -60,7 +60,7 @@ class Portfolio extends Component {
 					                        <img
 					                          alt="..."
 					                          className="align-middle border-none max-w-full h-auto rounded-lg"
-					                          src={require("assets/img/landing.jpg")}
+					                          src="https://demos.creative-tim.com/notus-react/static/media/landing.37b6ed3b.jpg"
 					                        />
 				                        </div>
 				                    </Link>

5daefa35a070d47b25cd8180eaa3223f89cce06c
[Vicente G. Reyes] <[vicentereyes.freelance@gmail.com]>
2020-11-18 09:27:32 +0800

    made font large for logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar(props) {
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Link
-              className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
+              className="text-white text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase"
               to="/"
             >
               Studio:icvn.tech

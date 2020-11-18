f2eda97a96a752536d7a6858c741b7b5ff0c0f5b
[Vicente G. Reyes] <[vicentereyes.freelance@gmail.com]>
2020-11-19 04:41:56 +0800

    hubspot form worked by withloveinternet

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import HubspotForm from 'react-hubspot-form'
 
 
 class Contact extends Component {
@@ -17,56 +18,13 @@ class Contact extends Component {
 			                      Complete this form and we will get back to you in 24
 			                      hours.
 			                    </p>
-			                    <div className="relative w-full mb-3 mt-8">
-			                      <label
-			                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
-			                        htmlFor="full-name"
-			                      >
-			                        Full Name
-			                      </label>
-			                      <input
-			                        type="text"
-			                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
-			                        placeholder="Full Name"
-			                      />
-			                    </div>
-
-			                    <div className="relative w-full mb-3">
-			                      <label
-			                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
-			                        htmlFor="email"
-			                      >
-			                        Email
-			                      </label>
-			                      <input
-			                        type="email"
-			                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
-			                        placeholder="Email"
-			                      />
-			                    </div>
-
-			                    <div className="relative w-full mb-3">
-			                      <label
-			                        className="block uppercase text-gray-700 text-xs font-bold mb-2"
-			                        htmlFor="message"
-			                      >
-			                        Message
-			                      </label>
-			                      <textarea
-			                        rows="4"
-			                        cols="80"
-			                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-			                        placeholder="Type a message..."
-			                      />
-			                    </div>
-			                    <div className="text-center mt-6">
-			                      <button
-			                        className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-			                        type="button"
-			                      >
-			                        Send Message
-			                      </button>
-			                    </div>
+								<HubspotForm
+								   portalId='8728743'
+								   formId='872e4a51-201b-4b51-8ac7-9a03eec561a1'
+								   onSubmit={() => console.log('Submit!')}
+								   onReady={(form) => console.log('Form ready!')}
+								   loading={<div>Loading...</div>}
+								   />
 		                    </div>
 		                </div>
 		              </div>

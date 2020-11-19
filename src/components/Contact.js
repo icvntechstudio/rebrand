ed477236ad8830621d7727ddca4fa48a1ec9caf4
[Vicente G. Reyes] <[vicentereyes.freelance@gmail.com]>
2020-11-20 05:42:38 +0800

    last edit ocntact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,11 +6,11 @@ class Contact extends Component {
 	render() {
 		return (
 	        <section className="relative block py-20 lg:pt-0 bg-gray-900">
-	            <div className="container mx-auto">
+	            <div className="container mx-auto px-4">
 		            <div className="flex flex-wrap justify-center lg:-mt-64 -mt-48">
-		                <div className="container w-full lg:w-full px-10">
-			                <div className="relative flex flex-auto min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 px-4 x-8">
-			                    <div className="flex-auto py-6">
+		                <div className="w-full lg:w-6/12 px-4">
+			                <div className="relative flex flex-auto min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 px-4 py-8">
+			                    <div className="flex-auto py-5 lg:p-10">
 				                    <h4 className="text-4xl font-bold text-center">
 				                      Want to work with us?
 				                    </h4>

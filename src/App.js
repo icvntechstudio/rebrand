e8f5c4ed3c10d0d8d50ad7b5025d90c450e7183f
Vicente G. Reyes <vicentereyes.freelance@gmail.com>
2020-12-01 00:26:16 +0800

    removed contentful, added page component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Header from './components/Header'
-import Feature from './components/Features'
-import Clients from './components/Clients'
-import Portfolio from './components/Portfolio'
-import Testimonial from './components/Testimonial'
-import Tech from './components/Tech'
-import About from './components/About'
-import Services from './components/Services'
-import Contact from './components/Contact'
+import Page from './components/Page'
 
 
 class App extends Component{
@@ -19,15 +11,7 @@ class App extends Component{
     return (
       <div>
         <Navbar />
-        <Header />
-        <Feature />
-        <Clients />
-        <Portfolio />
-        <Testimonial />
-        <Tech />
-        <About />
-        <Services />
-        <Contact />
+        <Page />
         <Footer />
       </div>
       )
---
+++ src/components/Page.js
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+
+
+export default class Page extends Component {
+  render() {
+    return (
+      <div>Heyyy</div>
+    )
+  }
+}

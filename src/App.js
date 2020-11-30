bfbeb0157ab296ae4f33331b6fb38dd80467f45b
Vicente G. Reyes <vicentereyes.freelance@gmail.com>
2020-12-01 03:37:37 +0800

    removed page and restored everything

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Page from './components/Page'
+import Header from './components/Header'
+import Feature from './components/Features'
+import Clients from './components/Clients'
+import Portfolio from './components/Portfolio'
+import Testimonial from './components/Testimonial'
+import Tech from './components/Tech'
+import About from './components/About'
+import Services from './components/Services'
+import Contact from './components/Contact'
 
 
 class App extends Component{
@@ -11,7 +19,15 @@ class App extends Component{
     return (
       <div>
         <Navbar />
-        <Page />
+        <Header />
+        <Feature />
+        <Clients />
+        <Portfolio />
+        <Testimonial />
+        <Tech />
+        <About />
+        <Services />
+        <Contact />
         <Footer />
       </div>
       )

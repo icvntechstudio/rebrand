3f2cde34acb6a9642a203f34f0bb636858d2ad6b
Vicente G. Reyes <vicentereyes.freelance@gmail.com>
2020-12-12 00:01:27 +0800

    partners done

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,75 +1,55 @@
-import React, { Component } from 'react'
+import React from 'react'
 import LottieFeature from '../components/Lottie/LottieFeature'
-import shopify from '../assets/lottie/shopify.json'
-import mailchimp from '../assets/lottie/mailchimp.json'
-import digitalOcean from '../assets/lottie/digitalOcean.json'
-import digitalEagles from '../assets/lottie/digitalEagles.json'
+import { useQuery } from "graphql-hooks"
 
 
-class Clients extends Component {
-	render() {
-		return (
-			<div className="bg-gray-300">
-	            <div className="container mx-auto">
-			        <section className="pt-20 pb-32">
-			          <div className="container mx-auto" id="about">
-			            <div className="flex flex-wrap justify-center text-center mb-24">
-			              <div className="w-full xl:w-8/12 lg:w-8/12 md:w-8/12 px-6">
-			                <h2 className="text-4xl font-semibold">Our Partners</h2>
-			                <p className="text-lg leading-relaxed m-4 text-gray-600">
-			                  We are privileged to be trusted by our partners to help us deliver excellence
-			                </p>
-			              </div>
-			            </div>
-			            <div className="flex flex-wrap justify-center items-center mx-auto p-8">
-			              <div className="w-full md:w-6/12 lg:w-4/12 sm:w-1/2 lg:mb-0 mb-12 px-6 mt-12">
-			                <div>
-												<LottieFeature lotti={shopify} height={110} width={100} />
-			                  <div className="pt-6 text-center">
-			                    <h5 className="text-xl font-bold">Shopify Partners</h5>
-			                  </div>
-			                </div>
-			              </div>
-			              <div className="w-full md:w-6/12 lg:w-4/12 sm:w-1/2 lg:mb-0 mb-12 px-6 mt-12">
-			                <div>
-												<img className="mx-auto" src="https://assets.freelancersunion.org/static/images/member-badge.dcf5a9180afa.png" alt="Proud member of Freelancers Union" height="110" width="100" />
-			                  <div className="pt-6 text-center">
-			                    <h5 className="text-xl font-bold">Freelance Union</h5>
-			                  </div>
-			                </div>
-			              </div>
-			              <div className="w-full md:w-6/12 lg:w-4/12 sm:w-1/2 lg:mb-0 mb-12 px-6 mt-12">
-			                <div>
-			                  <LottieFeature lotti={mailchimp} height={110} width={100} />
-			                  <div className="pt-6 text-center">
-			                    <h5 className="text-xl font-bold">Mailchimp & Co.</h5>
-			                  </div>
-			                </div>
-			              </div>
-										<div className="w-full md:w-6/12 lg:w-4/12 sm:w-1/2 lg:mb-0 mb-12 px-6 mt-12">
-			                <div>
-												<LottieFeature lotti={digitalOcean} height={110} width={110} />
-			                  <div className="pt-6 text-center">
-			                    <h5 className="text-xl font-bold">Digital Ocean</h5>
-			                  </div>
-			                </div>
-			              </div>
-										<div className="w-full md:w-6/12 lg:w-4/12 sm:w-1/2 lg:mb-0 mb-12 px-6 mt-12">
-			                <div>
-												<LottieFeature lotti={digitalEagles} height={110} width={110} />
-			                  <div className="pt-6 text-center">
-			                    <h5 className="text-xl font-bold">Digital Eagles</h5>
-			                  </div>
-			                </div>
-			              </div>
-			            </div>
-			          </div>
-			        </section>
-	        </div>
+const PARTNER_QUERY = `query {
+  allPartners {
+    name
+    icon
+    website
+  }
+}`
+
+
+export default function Clients() {
+	const { loading, error, data } = useQuery(PARTNER_QUERY, {
+		variables: {
+			limit: 4
+		}
+	})
+
+	if (loading) return "Loading..."
+	if (error) return "Something Bad Happened"
+
+	return (
+		<div className="bg-gray-300">
+			<div className="container mx-auto">
+				<section className="pt-20 pb-32">
+					<div className="container mx-auto" id="about">
+						<div className="flex flex-wrap justify-center text-center mb-24">
+							<div className="w-full xl:w-8/12 lg:w-8/12 md:w-8/12 px-6">
+								<h2 className="text-4xl font-semibold">Our Partners</h2>
+								<p className="text-lg leading-relaxed m-4 text-gray-600">
+			            We are privileged to be trusted by our partners to help us deliver excellence
+			          </p>
+							</div>
+						</div>
+						<div className="flex flex-wrap justify-center items-center mx-auto p-8">
+							{data.allPartners.map((partner) => (
+								<div className="w-full md:w-6/12 lg:w-4/12 sm:w-1/2 lg:mb-0 mb-12 px-6 mt-12">
+										<div key={partner.id}>
+											<LottieFeature lotti={partner.icon} height={110} width={100} />
+										</div>
+										<div className="pt-6 text-center">
+											<h5 className="text-xl font-bold">{partner.name}</h5>
+										</div>
+								</div>
+							))}
+						</div>
+					</div>
+				</section>
 			</div>
-
-		)
-	}
+		</div>
+	)
 }
-
-export default Clients
